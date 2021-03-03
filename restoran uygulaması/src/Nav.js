f95494be6993fccd09f5217d8837a55116becd55
Emir <emirmatik16@gmail.com>
2021-03-03 15:55:21 +0300

    Update Nav.js

--- restoran uygulaması/src/Nav.js
+++ restoran uygulaması/src/Nav.js
@@ -5,7 +5,6 @@ export default function Nav({restaurants, setRestaurants}) {
    const fetchRestaurant = async () => {
       const res = await fetch("https://random-data-api.com/api/restaurant/random_restaurant");
       const data = await res.json();
-      console.log(data);
       setRestaurants([...restaurants, data]);
    }
 
